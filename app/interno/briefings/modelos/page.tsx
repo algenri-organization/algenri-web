39fca6f61fe424492f57ca8ba6bce77f865f5f11
michelmakiak-ux <michel.makiak@gmail.com>
2026-09-05 09:03:55 -0300

    feat: add internal briefing models page

---
+++ app/interno/briefings/modelos/page.tsx
@@ -0,0 +1,10 @@
+import BriefingTemplateAdmin from "@/components/briefing/briefing-template-admin";
+
+export const metadata = {
+  title: "Modelos de Briefing | ALGENRI",
+  robots: { index: false, follow: false },
+};
+
+export default function BriefingModelsPage() {
+  return <BriefingTemplateAdmin />;
+}

c9b5d1ad0b79550a80608a3b51f99e0cedba135f
michelmakiak-ux <michel.makiak@gmail.com>
2026-09-06 20:17:04 -0300

    feat: add launch readiness internal page

---
+++ app/interno/prontidao/page.tsx
@@ -0,0 +1,10 @@
+import LaunchReadiness from "@/components/internal/launch-readiness";
+
+export const metadata = {
+  title: "Prontidão para Vendas | ALGENRI",
+  robots: { index: false, follow: false },
+};
+
+export default function LaunchReadinessPage() {
+  return <LaunchReadiness />;
+}

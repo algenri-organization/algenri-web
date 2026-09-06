c24b737328b9d57d14cf606ea016be53482e925a
michelmakiak-ux <michel.makiak@gmail.com>
2026-09-06 19:40:37 -0300

    feat: add internal contracts page

---
+++ app/interno/contratos/page.tsx
@@ -0,0 +1,4 @@
+import ContractsAdmin from "@/components/contracts/contracts-admin";
+
+export const metadata={title:"Contratos | ALGENRI",robots:{index:false,follow:false}};
+export default function ContractsPage(){return <ContractsAdmin/>;}

d1a36d769192a5e3973b355ea0fba5b68da33ffb
michelmakiak-ux <michel.makiak@gmail.com>
2026-09-06 16:54:02 -0300

    feat: integrate proposals with project page

--- app/interno/projetos/[id]/page.tsx
+++ app/interno/projetos/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ProjectDetailAdmin from "@/components/client-flow/project-detail-admin";
+import ProjectProposalsPanel from "@/components/proposals/project-proposals-panel";
 
 export const metadata = {
   title: "Projeto | ALGENRI",
@@ -7,5 +8,5 @@ export const metadata = {
 
 export default async function ProjectPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-  return <ProjectDetailAdmin id={id} />;
+  return <><ProjectDetailAdmin id={id} /><ProjectProposalsPanel projectId={id} /></>;
 }

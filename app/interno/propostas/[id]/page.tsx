ad59262d1ffaf0fbc6ed531179c787d7f5437f3c
michelmakiak-ux <michel.makiak@gmail.com>
2026-09-07 00:12:51 -0300

    feat: show contract continuation after proposal approval

--- app/interno/propostas/[id]/page.tsx
+++ app/interno/propostas/[id]/page.tsx
@@ -1,9 +1,10 @@
 import ProposalDetailAdmin from "@/components/proposals/proposal-detail-admin";
 import ProposalDocumentActions from "@/components/proposals/proposal-document-actions";
+import ProposalToContractAction from "@/components/contracts/proposal-to-contract-action";
 
 export const metadata={title:"Proposta Comercial | ALGENRI",robots:{index:false,follow:false}};
 
 export default async function ProposalPage({params}:{params:Promise<{id:string}>}){
   const {id}=await params;
-  return <><ProposalDetailAdmin id={id}/><ProposalDocumentActions id={id}/></>;
+  return <><ProposalDetailAdmin id={id}/><ProposalToContractAction id={id}/><ProposalDocumentActions id={id}/></>;
 }
